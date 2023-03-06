e62e0b7ebebb70cb1e2e25fc15ab6354b6a51124
mpalanis <mateusz_palanis@vp.pl>
2023-03-06 15:24:17 +0100

    Init commit, add type script, jest (test library), and simple routes.

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express, {json} from "express";
+import cors from 'cors';
+import 'express-async-errors';
+import rateLimit from "express-rate-limit";
+import {adRouter} from "./routes/ad.router";
+import {handleError} from "./utils/errors";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+
+app.use('/ad', adRouter);
+
+app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3001');
+});
---
+++ routes/ad.router.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+
+/* GET users listing. */
+export const adRouter = Router()
+    .get('/', async (req, res) => {
+      console.log("No elo");
+    })
+
